a2fca657400f954d988c03555373f6c986fd220d
Jonas Näsman <jonas.nasman@consid.se>
2026-03-12 13:25:38 +0100

    Create content-page.blade.php

---
+++ source/views/content-page.blade.php
@@ -0,0 +1,69 @@
+@extends('templates.master')
+
+@section('layout')
+    <div class="c-content-page">
+        <div class="c-content-page__grid">
+            <div class="c-content-page__main">
+                <div class="o-container c-content-page__main-inner">
+                    <div class="c-content-page__helper u-print-display--none">
+                        @includeIf('partials.navigation.breadcrumb')
+
+                        <div class="c-content-page__print" role="menubar" aria-label="{{ __('Sidfunktioner', 'lidingo-customisation') }}">
+                            @button([
+                                'text' => __('Skriv ut', 'lidingo-customisation'),
+                                'style' => 'outlined',
+                                'color' => 'primary',
+                                'href' => '#',
+                                'icon' => null,
+                                'classList' => ['c-content-page__print-button'],
+                                'attributeList' => [
+                                    'onClick' => 'window.print();return false;',
+                                    'aria-label' => __('Skriv ut', 'lidingo-customisation'),
+                                ]
+                            ])
+                            @endbutton
+                        </div>
+                    </div>
+
+                    {!! $hook->innerLoopStart !!}
+
+                    @php($pageTitle = method_exists($post, 'getTitle') ? $post->getTitle() : ($post->post_title ?? ''))
+
+                    @if (!empty($pageTitle))
+                        <h1 class="c-content-page__title">{!! $pageTitle !!}</h1>
+                    @endif
+
+                    @if (!empty($contentPagePreamble))
+                        <div class="c-content-page__preamble lead">
+                            {!! $contentPagePreamble !!}
+                        </div>
+                    @endif
+
+                    <div class="c-content-page__content">
+                        @if ($hasBlocks && $post)
+                            {!! $post->postContentFiltered !!}
+                        @endif
+                    </div>
+
+                    @if (!empty($contentPagePublishedDate))
+                        <p class="c-content-page__published">
+                            {{ __('Publiceringsdatum', 'lidingo-customisation') }}: {{ $contentPagePublishedDate }}
+                        </p>
+                    @endif
+
+                    {!! $hook->innerLoopEnd !!}
+                </div>
+            </div>
+
+            <aside class="c-content-page__aside">
+                <div class="c-content-page__aside-inner">
+                    @includeIf('partials.sidebar', ['id' => 'right-sidebar'])
+                </div>
+            </aside>
+        </div>
+
+        <div class="o-container c-content-page__below">
+            @includeIf('partials.sidebar', ['id' => 'content-area', 'classes' => ['o-grid']])
+        </div>
+    </div>
+@stop
