73088be61fa673116ab9265890755c382438f983
Jonas Näsman <jonas.nasman@consid.se>
2026-05-28 12:54:33 +0200

    Render place address as primary + secondary lines

--- source/views/v3/partials/schema/event/place-card-lidingo.blade.php
+++ source/views/v3/partials/schema/event/place-card-lidingo.blade.php
@@ -1,6 +1,10 @@
 @card([
     'heading' => $lang->placeTitle,
 ])
+    @php($placeAddressLines = preg_split('/<br\s*\/?>/i', (string) $place['address']) ?: [])
+    @php($placeAddressLines = array_values(array_filter(array_map('trim', $placeAddressLines), static fn ($line) => $line !== '')))
+    @php($placeAddressPrimary = $placeAddressLines[0] ?? '')
+    @php($placeAddressSecondary = array_slice($placeAddressLines, 1))
     @if(!empty($place['lat']) && !empty($place['lng']))
         @slot('beforeContent')
             @map([
@@ -22,17 +26,34 @@
 
     @if(!empty($place['address']))
         @slot('aboveContent')
-            @typography([
-                'element' => 'p',
-                'variant' => 'p',
-                'classList' => [
-                    'c-card__content',
-                    'c-event-page__place-address'
-                ]
-            ])
-                <span class="c-event-page__place-address-icon" aria-hidden="true">:platsnål:</span>
-                <span class="c-event-page__place-address-text">{!! $place['address'] !!}</span>
-            @endtypography
+            <div class="c-event-page__place-address-block">
+                @if(!empty($placeAddressPrimary))
+                    @typography([
+                        'element' => 'p',
+                        'variant' => 'p',
+                        'classList' => [
+                            'c-card__content',
+                            'c-event-page__place-address'
+                        ]
+                    ])
+                        <span class="c-event-page__place-address-icon" aria-hidden="true">:platsnål:</span>
+                        <span class="c-event-page__place-address-text">{!! $placeAddressPrimary !!}</span>
+                    @endtypography
+                @endif
+
+                @if(!empty($placeAddressSecondary))
+                    @typography([
+                        'element' => 'p',
+                        'variant' => 'p',
+                        'classList' => [
+                            'c-card__content',
+                            'c-event-page__place-address-details'
+                        ]
+                    ])
+                        {!! implode('<br>', $placeAddressSecondary) !!}
+                    @endtypography
+                @endif
+            </div>
         @endslot
     @endif
 
